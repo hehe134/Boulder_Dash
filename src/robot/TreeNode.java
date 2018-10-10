a1f485f0571fc323ade9509352220bbe589cdf12
heh134 <yzqwzq@qq.com>
2018-10-11 00:38:47 +0300

    Game

---
+++ src/robot/TreeNode.java
@@ -0,0 +1,49 @@
+package robot;
+
+import java.util.List;
+
+public class TreeNode implements Cloneable {
+    List<TreeNode> childList;
+
+    int key;
+    char direction;
+    //        private TreeNode child;
+    TreeNode parent;
+    boolean close;
+    Game myGame;
+
+    public TreeNode(int key, char direction, boolean close, List<TreeNode> childList,
+                    TreeNode parent, Game myGame) {
+        this.key = key;
+        this.childList = childList;
+        this.parent = parent;
+        this.close = close;
+        this.direction = direction;
+        this.myGame = myGame;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public Game getMyGame() {
+        return myGame;
+    }
+
+    public void setClose() {
+        close = true;
+    }
+
+    public TreeNode getParent() {
+        return parent;
+    }
+
+    public char getDirection() {
+        return direction;
+    }
+
+    @Override
+    public Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
