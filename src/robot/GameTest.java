b882e10a7e62f67369df895f2a8e994fab40c193
heh134 <yzqwzq@qq.com>
2018-10-13 11:44:41 +0300

    resolved problem Astar

--- src/robot/GameTest.java
+++ src/robot/GameTest.java
@@ -37,21 +37,5 @@ public class GameTest {
 
         AStar myAstar = new AStar();
         System.out.println("result2: " + myAstar.getWholePath());
-
-
-//        myGame.turnLeft();
-//        myGame.turnLeft();
-//        myGame.turnDown();
-//        myGame.turnLeft();
-//        System.out.println("Robot: (" + myGame.robot.getX() + " , " + myGame.robot.getY() + ")");
-//
-//        if (myGame.win) System.out.println("You win");
-//        System.out.println("Score: "+myGame.score);
-//        for (int j = 0; j < myGame.n; j++) {
-//            for (int i = 0; i < myGame.m; i++) {
-//                System.out.print(myGame.map[i][j]);
-//            }
-//            System.out.println();
-//        }
     }
 }
